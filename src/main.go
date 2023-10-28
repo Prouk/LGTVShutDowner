c3a41a502d738373f2d4dbcd9d8a04058d98a08a
Prouk <valentin.tahon2@gmail.com>
2023-10-28 05:08:47 +0200

    Send custom command to LGTV based on input flag
    Add a flag to the main function that allows the user to specify the command to send to the LGTV. The default command is set to 'PowerOn'.

--- src/main.go
+++ src/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"flag"
 	"github.com/Prouk/LGTVShutDowner/pkg"
 	"log"
 )
 
+var cmd string
+
 func main() {
+	flag.StringVar(&cmd, "c", "PowerOn", "The command to send to send to the TV (default : 'PowerOn')")
 	lsd, err := pkg.NewLsd()
 	if err != nil {
 		log.Fatal(err)
@@ -18,7 +22,7 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = lsd.SendCommand("PowerOff")
+	err = lsd.SendCommand(cmd)
 	if err != nil {
 		log.Fatal(err)
 	}
